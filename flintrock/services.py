d9d6f29fe00c933e483801cff2c41d242b3fd1a1
123gpg321 <123gpg321@gmail.com>
2020-11-25 16:25:22 +0000

    flintrock v1.0.0 with increased hdfs spark timeouts launch

--- flintrock/services.py
+++ flintrock/services.py
@@ -196,7 +196,7 @@ class HDFS(FlintrockService):
             """)
 
         # This loop is a band-aid for: https://github.com/nchammas/flintrock/issues/157
-        attempt_limit = 3
+        attempt_limit = 6
         for attempt in range(attempt_limit):
             try:
                 ssh_check_output(
@@ -214,7 +214,7 @@ class HDFS(FlintrockService):
                             )"
                         done
                     """.format(m=shlex.quote(cluster.master_host)),
-                    timeout_seconds=90
+                    timeout_seconds=240
                 )
                 break
             except socket.timeout as e:
@@ -376,7 +376,7 @@ class Spark(FlintrockService):
         logger.info("[{h}] Configuring Spark master...".format(h=host))
 
         # This loop is a band-aid for: https://github.com/nchammas/flintrock/issues/129
-        attempt_limit = 3
+        attempt_limit = 6
         for attempt in range(attempt_limit):
             try:
                 ssh_check_output(
@@ -395,7 +395,7 @@ class Spark(FlintrockService):
                             )"
                         done
                     """.format(m=shlex.quote(cluster.master_host)),
-                    timeout_seconds=90
+                    timeout_seconds=240
                 )
                 break
             except socket.timeout as e:
